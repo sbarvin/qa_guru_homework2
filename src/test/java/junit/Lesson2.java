7521a8fadf4c6396b02f3f1b670d6d5df8c6fdad
xoliwar <95bsk@mail.ru>
2022-04-24 16:35:50 +0300

    add Selenoid

--- src/test/java/junit/Lesson2.java
+++ src/test/java/junit/Lesson2.java
@@ -3,6 +3,7 @@ package junit;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 
@@ -17,6 +18,12 @@ public class Lesson2 {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @Test
