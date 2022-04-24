423961deb1449e316c2de753436987da684edb18
xoliwar <95bsk@mail.ru>
2022-04-24 16:42:44 +0300

    add Attach

--- src/test/java/junit/Lesson2.java
+++ src/test/java/junit/Lesson2.java
@@ -2,7 +2,9 @@ package junit;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -74,5 +76,14 @@ public class Lesson2 {
                 text("Russia, Republic of Bashkortostan"),
                 text("NCR Noida"));
 
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 }
